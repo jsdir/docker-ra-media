92daf452c6e0884797e29ff309aec682f34fcc5d
Jason Sommer <jsdirv@gmail.com>
2015-05-27 00:40:19 -0500

    Added push commands

--- docker-ra-media-build/build.sh
+++ docker-ra-media-build/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker build -t jsdir/ra-media-build:0.1.0
+docker push jsdir/ra-media-build
--- docker-ra-media/build.sh
+++ docker-ra-media/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker build -t jsdir/ra-media:0.1.0 .
+docker push jsdir/ra-media
